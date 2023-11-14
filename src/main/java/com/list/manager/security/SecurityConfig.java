fe990bdad2ebfe93683ece5f7cd4a54f032202c7
Tastyfruitloops <aleshka.lobanov.1703@gmail.com>
2023-11-14 23:48:56 +0500

    entities access added

--- src/main/java/com/list/manager/security/SecurityConfig.java
+++ src/main/java/com/list/manager/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.list.manager.security;
 
 
+import com.list.manager.security.filter.AccessFilter;
 import com.list.manager.security.filter.CookieAuthFilter;
 import com.list.manager.security.filter.UserPresenceFilter;
 import org.springframework.context.annotation.Bean;
@@ -18,10 +19,12 @@ public class SecurityConfig {
 
     private final UserAuthEntryPoint userAuthEntryPoint;
     private final UserAuthProvider userAuthProvider;
+    private final UserAccessProvider userAccessProvider;
 
-    public SecurityConfig(UserAuthEntryPoint userAuthEntryPoint, UserAuthProvider userAuthProvider) {
+    public SecurityConfig(UserAuthEntryPoint userAuthEntryPoint, UserAuthProvider userAuthProvider, UserAccessProvider userAccessProvider) {
         this.userAuthEntryPoint = userAuthEntryPoint;
         this.userAuthProvider = userAuthProvider;
+        this.userAccessProvider = userAccessProvider;
     }
 
 
@@ -30,7 +33,8 @@ public class SecurityConfig {
         http
                 .exceptionHandling().authenticationEntryPoint(userAuthEntryPoint)
                 .and()
-                .addFilterBefore(new UserPresenceFilter(userAuthProvider), BasicAuthenticationFilter.class)
+                .addFilterBefore(new AccessFilter(userAccessProvider), BasicAuthenticationFilter.class)
+                .addFilterBefore(new UserPresenceFilter(userAuthProvider), AccessFilter.class)
                 .addFilterBefore(new CookieAuthFilter(userAuthProvider), UserPresenceFilter.class)
                 .csrf().disable()
                 .logout().deleteCookies(CookieAuthFilter.COOKIE_NAME)
