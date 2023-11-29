bfe8c0f144801ce3cf3a7eec89849a2b4bb20acf
Moultree <okaykudes@gmail.com>
2023-11-29 17:47:58 +0600

    Reformat logger output

--- src/main/java/com/dev/listmanager/service/LoggingService.java
+++ src/main/java/com/dev/listmanager/service/LoggingService.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,12 +20,26 @@ public class LoggingService implements ILoggingService {
     public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object body) {
         StringBuilder stringBuilder = new StringBuilder();
 
-        stringBuilder.append("REQUEST ");
-        stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
-        stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
-        stringBuilder.append("RESPONSE ");
-        stringBuilder.append("code=").append(httpServletResponse.getStatus());
+        String method = httpServletRequest.getMethod();
+        String path = httpServletRequest.getRequestURI();
+        int statusCode = httpServletResponse.getStatus();
+        String statusMessage = HttpStatus.valueOf(statusCode).getReasonPhrase();
 
+        stringBuilder.append("\u001B[37m\"").append(method).append(" ").append(path).append("\"\u001B[0m ");
+
+        // Set color based on the status code range
+        if (statusCode >= 200 && statusCode < 300) {
+            stringBuilder.append("\u001B[32m"); // Green color
+        } else if (statusCode >= 300 && statusCode < 400) {
+            stringBuilder.append("\u001B[33m"); // Yellow color
+        } else if (statusCode >= 400 && statusCode < 500) {
+            stringBuilder.append("\u001B[31m"); // Red color
+        } else {
+            stringBuilder.append("\u001B[0m"); // Default color
+        }
+
+        // Append the status code and reset the color
+        stringBuilder.append(statusCode).append(" ").append(statusMessage).append(" \u001B[0m"); // Reset color
         LOGGER.info(stringBuilder.toString());
     }
 }
